b1b8f6bd3cb76f4e2231ecb5c16cbc9107ea8a00
rajeshbasnet <rajeshkhapataribasnet123@gmail.com>
2025-09-23 11:24:47 +0545

    Added code for climbingstairs problem

--- LeetCode/src/Leetcode/ClimbingStairs.java
+++ LeetCode/src/Leetcode/ClimbingStairs.java
@@ -1,11 +1,25 @@
 package Leetcode;
 
 public class ClimbingStairs {
-    public static int climbStairs(int n) {
+    public static int climbStairsRecursive(int n) {
         if(n <= 2) return n;
         // recursive
-        int ways = climbStairs(n - 1) + climbStairs(n - 2);
-        return ways;
+        //We can store array value for all value less than n to do lookup next time from array
+        return climbStairsRecursive(n - 1) + climbStairsRecursive(n - 2);
+    }
+
+    public static int climbStairs(int n) {
+        if(n <= 2) return n;
+        int lastOne = 1;
+        int lastTwo = 2;
+
+        for(int i = 3; i <= n; i++) {
+            int current = lastOne + lastTwo;
+            lastOne = lastTwo;
+            lastTwo = current;
+        }
+
+        return lastTwo;
     }
 
     public static void main(String[] args) {
